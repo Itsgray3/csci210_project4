a5ad083bcc7aaf8dc3c8fbc46a6b9e06288d9f7c
Gray <itsgraythehuman@gmail.com>
2024-12-04 22:17:21 -0700

    update

--- rsh.c
+++ rsh.c
@@ -92,7 +92,7 @@ int main(int argc, char **argv) {
     // DONE:
     // create the message listener thread
 
-	pthread_create(pid, NULL, &messageListener, uName);
+	pthread_create(&pid, NULL, &messageListener, uName);
 
 
 
